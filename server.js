cbab7aed6e8a637109bdf81e0623e9894a8c1b6b
Bipin Thapa <yukichin638@gmail.com>
2025-08-24 09:26:26 +0545

    response added for base link

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ server.use(cors());
 server.use(express.json());
 server.use('/email',email);
 
+server.get('/',(req,res)=>{
+  res.send("backend is working");
+})
+
 server.listen('5000',()=>{
   console.log(`server running at port 5000`);
 })
